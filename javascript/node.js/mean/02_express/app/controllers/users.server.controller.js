370506629391db059f0b20a8f9e77c9b7c7cf225
Sorel Mitra <smitra@sorel-mac-mini.local>
2016-04-19 17:23:54 +0300

    Load the User model and add some comments.

--- javascript/node.js/mean/02_express/app/controllers/users.server.controller.js
+++ javascript/node.js/mean/02_express/app/controllers/users.server.controller.js
@@ -1,9 +1,14 @@
-var db = require('../../config/mongoose');
+// User is a sort of a class. It is returned by the mongoose library, via its model() function, that gets the data model name. In this case, the data model name is User, which we created in users.server.model.js and registered with mongoose in config/mongoose.js
+var User = require('mongoose').model('User');
 
 exports.create = function(req, res, next) {
-    res.json(200, {
-        message: "Will be implemented soon"
-    });
+    var user = new User(req.body);
+    user.save(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.json(user);
+    })
 };
 
 exports.get = function(req, res, next) {
