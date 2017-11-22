174a8edf48e4eadfc25635ad10e794e4379d9911
Sorel Mitra <sorelmitra@yahoo.com>
2017-11-22 18:53:03 +0200

    Add security for Actuator. Change Jersey application path to something different than '/' so that it does not interfere with Actuator. Add Consul IP address trying to make it work over VPN, but for some reason the PC in VPN is not accessible from the PCs from the local network - only the reverse is true.

--- java/spring/spring-cloud/boatdirectory/src/main/java/com/yahoo/sorelmitra/springcloud/boatdirectory/JerseyConfig.java
+++ java/spring/spring-cloud/boatdirectory/src/main/java/com/yahoo/sorelmitra/springcloud/boatdirectory/JerseyConfig.java
@@ -6,7 +6,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
