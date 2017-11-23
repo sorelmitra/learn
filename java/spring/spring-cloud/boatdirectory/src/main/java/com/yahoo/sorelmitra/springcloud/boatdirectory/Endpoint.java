d31950cb20e67463225c5cce5086ac440d9080a8
Sorel Mitra <sorelmitra@yahoo.com>
2017-11-23 19:48:24 +0200

    Put back dependency on spring-cloud-starter-consul-discovery to allow the component to register. Connect to localhost Consul by default. Add log when boatsNames() is called.

--- java/spring/spring-cloud/boatdirectory/src/main/java/com/yahoo/sorelmitra/springcloud/boatdirectory/Endpoint.java
+++ java/spring/spring-cloud/boatdirectory/src/main/java/com/yahoo/sorelmitra/springcloud/boatdirectory/Endpoint.java
@@ -5,16 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/boats")
 public class Endpoint {
+	private static Logger LOGGER = Logger.getLogger(Endpoint.class);
+
 
     @Path("/names")
     @Produces({ MediaType.APPLICATION_JSON })
     @GET
     public String[] boatsNames() {
+    		LOGGER.info("called");
         return new String[] { "HMS Belfast", "Cutty Sark" };
     }
 }
