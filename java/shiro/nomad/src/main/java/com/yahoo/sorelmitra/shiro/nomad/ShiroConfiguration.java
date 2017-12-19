56477f7f91e3fb11fefa724c5e20fd0f85fb4a43
Sorel Mitra <sorelmitra@yahoo.com>
2017-12-19 15:15:53 +0200

    Take a look at whether the SessionManager and SessionDAO interfaces allow you to search by session attribute. They don't.

--- java/shiro/nomad/src/main/java/com/yahoo/sorelmitra/shiro/nomad/ShiroConfiguration.java
+++ java/shiro/nomad/src/main/java/com/yahoo/sorelmitra/shiro/nomad/ShiroConfiguration.java
@@ -2,6 +2,9 @@ package com.yahoo.sorelmitra.shiro.nomad;
 
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.DefaultSessionManager;
+import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,10 @@ public class ShiroConfiguration {
 	@Bean
 	public SecurityManager securityManager() {
 		DefaultSecurityManager securityManager = new DefaultSecurityManager();
+		DefaultSessionManager sessionManager = new DefaultSessionManager();
+		securityManager.setSessionManager(sessionManager);
+		SessionDAO sessionDAO = new EnterpriseCacheSessionDAO();
+		sessionManager.setSessionDAO(sessionDAO);
 		return securityManager;
 	}
 
