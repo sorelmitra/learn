ce02c7639ee4855817d1b0d5fd706ebcefc21f52
Sorel Mitra <sorelmitra@yahoo.com>
2019-03-20 12:44:40 +0200

    Step advance in tutorial

--- javascript/node.js/codemix/tutorial-node101/src/main.js
+++ javascript/node.js/codemix/tutorial-node101/src/main.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const server = express();
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Example server listening on port ${port}!`)
+});
