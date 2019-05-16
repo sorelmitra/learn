9b83d633c31ac7efdc58ff79a59a1ec2c0088ed2
Sorel Mitra <sorelmitra@yahoo.com>
2019-05-16 18:50:47 +0300

    Add configurations for dev-local and dev-cloud. Add npm scripts for starting the django chatapi server.

--- src/chatmob/config.js
+++ src/chatmob/config.js
@@ -1,3 +1,4 @@
 
-console.log("Environment: ", process.env['REACT_APP_BOTAGG_MOB_ENV']);
-export default config = require('./config-dev.json');
+console.log("Environment: ", process.env.REACT_APP_BOTAGG_MOB_ENV);
+var configName = 'config-' + process.env.REACT_APP_BOTAGG_MOB_ENV;
+export default config = require(`./${configName}.json`);
