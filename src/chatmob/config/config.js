02a40d05602fc6eeb4b2ac210ae9ecd25c1c8912
Sorel Mitra <sorelmitra@yahoo.com>
2019-06-21 15:29:10 +0300

    Move chat name setting outside of JSON config to allow different values per app

--- src/chatmob/config/config.js
+++ src/chatmob/config/config.js
@@ -1,4 +1,8 @@
 
 console.log("Environment: ", process.env.REACT_APP_BOTAGG_MOB_ENV);
 var configName = 'config-' + process.env.REACT_APP_BOTAGG_MOB_ENV;
-exports.config = require(`./${configName}.json`);
+config = require(`./${configName}.json`);
+config['chat']['name'] = process.env.BOTAGG_CHAT_NAME;
+console.log('Config: ', config);
+
+exports.config = config;
