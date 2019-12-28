4ec277299cb1250a709d87746621a8acf316ec8c
Sorel Mitra <sorelmitra@yahoo.com>
2019-12-28 19:31:37 +0200

    verifit/fix getting test script path

--- verifit/src/verifit/verifit.py
+++ verifit/src/verifit/verifit.py
@@ -1,7 +1,15 @@
 import inspect, os, subprocess, sys
 
 def script_path(filename):
-	return os.path.join(sys.path[0], filename)
+	#dir = sys.path[0] # uses Python Path entries, not reliable
+	#dir = "test/jsonplaceholder" # Hardcoded
+	# First index is stack function, second index is file name.
+	# Partially reliable, works only if
+	# - this function is called from within another function from this file,
+	#   which in turn is called directly from the test file.
+	# Best solution I have so far.
+	dir = os.path.dirname(inspect.stack()[2][1])
+	return os.path.join(dir, filename)
 
 def load_file_as_string(filepath):
 	with open(filepath) as f:
@@ -11,7 +19,19 @@ def load_file_as_string(filepath):
 def run_command(command):
 	subprocess.run(command)
 
-def run_test(command, input_filename, output_filename, expected_output_filename):
+def get_input_filename(name):
+	return script_path(f"{name}.json")
+
+def get_output_filename(name):
+	return script_path(f"{name}-answer.json")
+
+def get_expected_output_filename(name):
+	return script_path(f"{name}-expected.json")
+
+def run_test(command, name):
+	input_filename = script_path(f"{name}.json")
+	output_filename = script_path(f"{name}-answer.json")
+	expected_output_filename = script_path(f"{name}-expected.json")
 	try:
 		os.unlink(output_filename)
 	except FileNotFoundError:
