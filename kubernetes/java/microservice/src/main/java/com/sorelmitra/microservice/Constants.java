fbe3053c44fb95a1948e94f02b30f6651a05ce95
Sorel Mitra <sorelmitra@yahoo.com>
2020-01-22 13:48:59 +0200

    kube/add crashing thread to test kube liveliness

---
+++ kubernetes/java/microservice/src/main/java/com/sorelmitra/microservice/Constants.java
@@ -0,0 +1,10 @@
+package com.sorelmitra.microservice;
+
+public class Constants {
+    static final String WORK_HORSE_1 = "Work Horse 1";
+    public static final String WORK_HORSE_2 = "Work Horse 2";
+    static final long SLEEP_TIME_MS_1 = 5000;
+    static final long SLEEP_TIME_MS_2 = 4500;
+    public static final int NO_CRASH = 0;
+    static final int TICKS_BEFORE_CRASH = 5;
+}
---
+++ kubernetes/java/microservice/src/main/java/com/sorelmitra/microservice/WorkHorse.java
@@ -0,0 +1,52 @@
+package com.sorelmitra.microservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkHorse implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WorkHorse.class);
+
+    private final Thread t;
+    private long sleepTimeMs;
+    private String name;
+    private boolean stopped;
+    private int ticksBeforeCrash;
+    private int ticks;
+
+    public WorkHorse(String name, long sleepTimeMs, int ticksBeforeCrash) {
+        this.name = name;
+        this.sleepTimeMs = sleepTimeMs;
+        this.ticksBeforeCrash = ticksBeforeCrash;
+        this.stopped = false;
+        this.ticks = 0;
+        t = new Thread(this);
+    }
+
+    public void start() {
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        while (!stopped) {
+            try {
+                Thread.sleep(sleepTimeMs);
+                if (shouldCrash()) {
+                    throw new NullPointerException(String.format("Crash by design in thread %s", name, ticks));
+                }
+                LOG.info("Thread {} ticked {}", name, ticks);
+            } catch (InterruptedException e) {
+                LOG.info("Thread {} ticked - interrupted", name);
+            }
+        }
+    }
+
+    private boolean shouldCrash() {
+        ticks++;
+        if (ticksBeforeCrash < 1) return false;
+        if (ticks <= ticksBeforeCrash) return false;
+        ticks = 0;
+        return true;
+    }
+}
