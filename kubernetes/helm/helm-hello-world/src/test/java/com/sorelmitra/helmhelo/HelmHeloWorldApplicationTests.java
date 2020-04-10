9465963bae9dd664394b0a549d32e9d1d8a32494
Sorel Mitra <sorelmitra@yahoo.com>
2020-04-10 18:54:50 +0300

    kube/helm - copy charts directory for deployment

--- kubernetes/helm/helm-hello-world/src/test/java/com/sorelmitra/helmhelo/HelmHeloWorldApplicationTests.java
+++ kubernetes/helm/helm-hello-world/src/test/java/com/sorelmitra/helmhelo/HelmHeloWorldApplicationTests.java
@@ -1,13 +1,5 @@
 package com.sorelmitra.helmhelo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HelmHeloWorldApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class TestHelmHeloWorldApplication {
 
 }
