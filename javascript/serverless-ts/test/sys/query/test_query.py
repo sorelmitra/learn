a5be5cccb401da97fadae4ec10dab8a4267f4851
Sorel Mitra <>
2021-07-13 20:05:29 +0300

    serverless/add test for the hello world

---
+++ javascript/serverless-ts/test/sys/query/test_query.py
@@ -0,0 +1,22 @@
+from verifit import *
+import re
+
+apiKey = "18yKKTwlod4V7bSRAK2wK2Vn1GObHPV13nUMXx4w"
+server = "39ijt5vs1a.execute-api.us-east-1.amazonaws.com/default"
+path = "pizzemQueryPizza"
+
+def test_query_pizza():
+	name = "query_pizza"
+
+	command = [
+		"curl", 
+		"-X", "GET",
+		"-H", f"x-api-key: {apiKey}",
+		"-H", "Content-Type: application/json",
+		f"https://{server}/{path}",
+		"--data", f"@{get_input_filename(name)}",
+		"-o", get_output_filename(name)
+	]
+
+	expected, got = run_test(command, name)
+	assert None != re.match(expected, got)
