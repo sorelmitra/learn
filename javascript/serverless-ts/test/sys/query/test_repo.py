375acf7f694ae2a03c91f8657a76b030abc4ef49
Sorel Mitra <>
2021-07-15 14:36:17 +0300

    serverless/rename sys test & fix regexp

--- javascript/serverless-ts/test/sys/query/test_repo.py
+++ javascript/serverless-ts/test/sys/query/test_repo.py
@@ -5,21 +5,22 @@ apiKey = "18yKKTwlod4V7bSRAK2wK2Vn1GObHPV13nUMXx4w"
 server = "https://5jej7wt976.execute-api.us-east-1.amazonaws.com/DEV"
 common_command = [
 	"curl", 
-	"-X", "GET",
 	"-H", f"x-api-key: {apiKey}",
 	"-H", "Content-Type: application/json",
 ]
 
 def test_query_pizza():
 	expected, actual = run_test(common_command + [
+		"-X", "GET",
 		f"{server}/pizzas",
 		"-o", get_output_filename(),
 	])
-	assert re.sub("\s+", "", expected) == re.sub("\s+", "", actual)
+	assert re.sub(r'\s+', "", expected) == re.sub(r'\s+', "", actual)
 
 def test_query_pizza_by_type():
 	expected, actual = run_test(common_command + [
+		"-X", "GET",
 		f"{server}/pizzas/:carbonara",
 		"-o", get_output_filename(),
 	])
-	assert re.sub("\s+", "", expected) == re.sub("\s+", "", actual)
+	assert re.sub(r'\s+', "", expected) == re.sub(r'\s+', "", actual)
