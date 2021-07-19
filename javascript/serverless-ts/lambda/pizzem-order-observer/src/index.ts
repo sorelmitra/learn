fc44c5f0df23a4d49ee1f3df85785cfa76e14922
Sorel Mitra <>
2021-07-19 20:10:25 +0300

    serverless/enqueue order email requests

--- javascript/serverless-ts/lambda/pizzem-order-observer/src/index.ts
+++ javascript/serverless-ts/lambda/pizzem-order-observer/src/index.ts
@@ -1,5 +1,14 @@
-import SNS from 'aws-sdk/clients/sns';
+import SQS from "aws-sdk/clients/sqs";
+import {OrderEmailQueue} from "./services/orderEmailQueue";
+import {ReceiptStoreQueue} from "../../pizzem-receipt-observer/src/services/receiptStoreQueue";
 
 export const handler = async (event, context) => {
-    console.log(event.Records);
+    console.log("Event records", event.Records);
+
+    let orderEmailQueue: OrderEmailQueue = new OrderEmailQueue(new SQS(), process.env.ORDER_EMAIL_QUEUE_URL);
+    try {
+        await orderEmailQueue.process(event.Records);
+    } catch (error) {
+        console.log(error);
+    }
 };
