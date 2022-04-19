f72d15d4b526d1aee660ac70a63c3d25843996b6
Sorel Mitra <>
2022-04-19 16:36:26 +0300

    verifit/support changing the stack number used to get test data from test function
    to allow running the test from common functions called from test functions

--- verifit/src/verifit/verifit.py
+++ verifit/src/verifit/verifit.py
@@ -69,7 +69,21 @@ LOG = Logger()
 #
 ###########################################################
 
+stack_number = 1
+stack_file_index = 1
+stack_function_index = 3
+
+def get_stack_number():
+    global stack_number
+    return stack_number
+
+def set_stack_number(number):
+    global stack_number
+    stack_number = number
+
 def script_path(filename):
+    global stack_number
+    global stack_file_index
     # Have tried several ways to get this path, including hardcoded and:
     # dir = sys.path[0] # uses Python Path entries, not reliable
     # This solution is based on reflection.
@@ -78,7 +92,7 @@ def script_path(filename):
     # - this function is called from within another function from this file,
     #   which in turn is called directly from the test file.
     # Best solution I have so far.
-    current_dir = os.path.dirname(inspect.stack()[2][1])
+    current_dir = os.path.dirname(inspect.stack()[stack_number + 1][stack_file_index])
     return os.path.join(current_dir, filename)
 
 
@@ -115,12 +129,16 @@ def get_other_filename(filename):
 
 
 def get_input_filename():
-    name = inspect.stack()[1][3]
+    global stack_number
+    global stack_function_index
+    name = inspect.stack()[stack_number][stack_function_index]
     return script_path(f"{name}.json")
 
 
 def get_output_filename():
-    name = inspect.stack()[1][3]
+    global stack_number
+    global stack_function_index
+    name = inspect.stack()[stack_number][stack_function_index]
     return script_path(f"{name}-answer.json")
 
 
@@ -137,7 +155,9 @@ def get_test_results(expected_output_filename, output_filename, update_snapshot)
 
 
 def run_test(command, update_snapshot=False):
-    name = inspect.stack()[1][3]
+    global stack_number
+    global stack_function_index
+    name = inspect.stack()[stack_number][stack_function_index]
     output_filename = script_path(f"{name}-answer.json")
     expected_output_filename = script_path(f"{name}-expected.json")
     try:
@@ -149,7 +169,9 @@ def run_test(command, update_snapshot=False):
 
 
 def run_triggered_background_test(background_test_command, trigger_command, update_snapshot=False):
-    name = inspect.stack()[1][3]
+    global stack_number
+    global stack_function_index
+    name = inspect.stack()[stack_number][stack_function_index]
     output_filename = script_path(f"{name}-answer.json")
     expected_output_filename = script_path(f"{name}-expected.json")
     try:
