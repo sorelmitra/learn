977122679a9c2120e0f2ed7be24e2004d08b0522
Sorel Mitra <sorelmitra@yahoo.com>
2024-03-20 11:56:24 +0200

    pytide/start generating realistic tide days

--- python/pytide/src/pytide.py
+++ python/pytide/src/pytide.py
@@ -1,8 +1,9 @@
+import datetime
 import random
 
 from src.tide_model import semidiurnal_tide, NEAP_MAX
 from src.tide_plot import plot_tide
-from src.tide_tables import TideHeight
+from src.tide_tables import TideDay, TideHeight
 
 tide_factors = dict(min_water_factor=2, max_water_factor=5)
 neap_level = NEAP_MAX
@@ -15,35 +16,47 @@ neaps_hw = compute_neaps_height(6)
 springs_lw = compute_springs_height(0)
 springs_hw = compute_springs_height(6)
 
+
+def reset_day(d=datetime.datetime.now()):
+	return d.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+
 # TODO: Make a generative function that goes both ways neaps <-> springs
-tide_heights = []
+tide_days = []
 tide_cycle_length = random.randint(7, 9)
 step = neap_level / (tide_cycle_length - 1)
 current_neap_level = neap_level
+a_date = reset_day()
+print(f"{a_date.strftime('%B')}")
+print()
 for k in range(0, tide_cycle_length):
 	# compute tide height based on neap level
 	compute_current_height = semidiurnal_tide(**tide_factors, neap_factor=current_neap_level)
 
 	# store tide height and function
-	tide = TideHeight(
-		compute_func=compute_current_height,
-		day_of_month=k + 1,
-		neap_level=current_neap_level,
-		hw_height=compute_current_height(6),
-		lw_height=compute_current_height(0),
+	hw = TideHeight(
+		time=a_date.time(),
+		height=compute_current_height(6),
+		life_cycle=TideHeight.HW
 	)
-	tide_heights.append(tide)
+	a_date = a_date + datetime.timedelta(hours=6)
+	lw = TideHeight(
+		time=a_date.time(),
+		height=compute_current_height(0),
+		life_cycle=TideHeight.LW
+	)
+	tide_day = TideDay(
+		compute_func=compute_current_height,
+		tide_date=a_date.date(),
+		neap_level=current_neap_level,
+		heights=[hw, lw]
+	)
+	a_date = a_date + datetime.timedelta(hours=6)
+	tide_days.append(tide_day)
+
 
 	# print tide height for the day
-	neap_level_string = f", neap level {format(current_neap_level, '.2f')}"
-	if tide.neap_level == neap_level:
-		neap_level_string = ' (neaps)'
-	elif tide.neap_level == 0:
-		neap_level_string = ' (springs)'
-	print(f"Day {tide.day}{neap_level_string}")
-	print(f"HW={format(tide.hw_height, '.1f')}")
-	print(f"LW={format(tide.lw_height, '.1f')}")
-	print()
+	tide_day.print()
 
 	# next neap level
 	current_neap_level = current_neap_level - step
@@ -53,8 +66,8 @@ for k in range(0, tide_cycle_length):
 
 a_tide_hour = 1 + 11 * random.random()
 tide_hour_per_hw = a_tide_hour - 6
-current_day = random.randint(0, len(tide_heights) - 1)
-current_tide = tide_heights[current_day]
+current_day = random.randint(0, len(tide_days) - 1)
+current_tide = tide_days[current_day]
 hw_sign = '' if tide_hour_per_hw < 0 else '+'
 hw_string = f"{hw_sign}{format(tide_hour_per_hw, '.1f')}" if abs(tide_hour_per_hw) >= 0.1 else ''
 tide_height_string = format(current_tide.compute_func(a_tide_hour), '.1f')
--- python/pytide/src/tide_tables.py
+++ python/pytide/src/tide_tables.py
@@ -1,17 +1,43 @@
-import datetime
+from src.tide_model import NEAP_MAX
 
 
 class TideHeight:
-	def __init__(self, *,
-				 compute_func,
-				 day_of_month, neap_level,
-				 hw_height, lw_height,
-				 hw_time=datetime.time(hour=0, minute=0),
-				 lw_time=datetime.time(hour=6, minute=0)):
-		self.day = day_of_month
+	LW = 'low water'
+	HW = 'high water'
+
+	# Constructs a tide height object
+	# @param time: the time of the tide height, a datetime.time() object
+	# @param height: the height of the tide, a float number representing meters
+	# @param life_cycle: the type of tide height (high or low water)
+	def __init__(self, *, time, height, life_cycle):
+		self.time = time
+		self.height = height
+		self.type = life_cycle
+
+	def print(self):
+		print(f"{self.time.strftime('%H%M')} {format(self.height, '.1f')}")
+
+
+class TideDay:
+	# Constructs a tide day object
+	# @param compute_func: the function that computes the tide height
+	# @param tide_date: the date of the tide, a datetime.date() object
+	# @param neap_level: the neap level of the tide, a float number between 0 (springs) and NEAP_MAX (neaps)
+	# @param heights: the tide heights for the day, a list of TideHeight objects; the constructor will sort them by time
+	def __init__(self, *, compute_func, tide_date, neap_level, heights):
+		self.date = tide_date
 		self.neap_level = neap_level
-		self.hw_height = hw_height
-		self.hw_time = hw_time
-		self.lw_height = lw_height
-		self.lw_time = lw_time
 		self.compute_func = compute_func
+		self.heights = heights
+		# sort heights by time
+		self.heights.sort(key=lambda x: x.time)
+
+	def print(self):
+		neap_level_string = f"neap level {format(self.neap_level, '.2f')}"
+		if self.neap_level == NEAP_MAX:
+			neap_level_string = 'neaps'
+		elif self.neap_level == 0:
+			neap_level_string = 'springs'
+		print(f"{self.date.day}, {self.date.strftime('%a')} ({neap_level_string})")
+		[tide_height.print() for tide_height in self.heights]
+		print()
