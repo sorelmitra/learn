7cf1c73c891072cc6207e696fb457514227450d5
Sorel Mitra <sorelmitra@yahoo.com>
2024-06-20 11:29:02 +0300

    dotnet/sst/improve handling of destroy everything request

--- dotnet/dotnet-sst-lambda/src/sst.config.ts
+++ dotnet/dotnet-sst-lambda/src/sst.config.ts
@@ -9,8 +9,14 @@ export default {
     };
   },
   stacks(app) {
-    if (process.env['DESTROY_EVERYTHING'] && app.stage !== "prod") {
-      console.log(`\n\n\n!!!\n!!!\n!!!\nWARNING: Requested to DESTROY ALL DATA!\nThis action will completely and IRREVERSIBLY DESTROY all DATA created with this app in STAGE ${app.stage}!!!\n!!!\n!!!\n!!!\n\n\n`);
+    if (process.env['DESTROY_EVERYTHING']) {
+      const awsProfile = process.env['AWS_PROFILE'];
+      if (app.stage.match(".*prod.*")
+        || (awsProfile && awsProfile.match(".*prod.*"))) {
+        console.log(`\n\nCowardly refusing to destroy all data for AWS_PROFILE ${awsProfile} and stage ${app.stage}, as at least one of them seems to be referring to production!\n\n`);
+      } else {
+        console.log(`\n\n\n!!!\n!!!\n!!!\nWARNING: Requested to DESTROY ALL DATA!\nThis action will completely and IRREVERSIBLY DESTROY all DATA created with this app in AWS_PROFILE ${awsProfile} and STAGE ${app.stage}!!!\n!!!\n!!!\n!!!\n\n\n`);
+      }
       app.setDefaultRemovalPolicy("destroy");
     }
     app.setDefaultFunctionProps({
