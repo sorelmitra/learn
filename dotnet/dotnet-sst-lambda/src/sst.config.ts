e314b29cf57f328d259e1d425f4bd289e0251e13
Sorel Mitra <sorelmitra@yahoo.com>
2024-06-20 11:16:02 +0300

    dotnet/sst/warn if requested to destroy everything

--- dotnet/dotnet-sst-lambda/src/sst.config.ts
+++ dotnet/dotnet-sst-lambda/src/sst.config.ts
@@ -9,6 +9,10 @@ export default {
     };
   },
   stacks(app) {
+    if (process.env['DESTROY_EVERYTHING'] && app.stage !== "prod") {
+      console.log(`\n\n\n!!!\n!!!\n!!!\nWARNING: Requested to DESTROY ALL DATA!\nThis action will completely and IRREVERSIBLY DESTROY all DATA created with this app in STAGE ${app.stage}!!!\n!!!\n!!!\n!!!\n\n\n`);
+      app.setDefaultRemovalPolicy("destroy");
+    }
     app.setDefaultFunctionProps({
       runtime: "dotnet8",
       timeout: 30,
