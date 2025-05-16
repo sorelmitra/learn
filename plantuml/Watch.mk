2596a5c8c74e869ba00855d1f6dcf0a4623615f6
Sorel Mitra <sorelmitra@yahoo.com>
2025-05-16 12:20:31 +0300

    plantuml/added watcher makefile sample

---
+++ plantuml/Watch.mk
@@ -0,0 +1,17 @@
+# Makefile for watching PlantUML files and generating diagrams on changes (macOS, local JAR)
+# Run:
+#   make watch-my-file
+# to watch my-file.puml and generate diagrams with your local plantuml.jar.
+#
+# Or run:
+#  make generate FILE=my-file.puml
+# to generate a diagram once.
+
+PLANTUML_JAR ?= /Users/sorel/Applications/plantuml-1.2025.2.jar
+
+watch-%:
+	fswatch -o $*.puml | xargs -n1 -I{} java -jar $(PLANTUML_JAR) $*.puml
+
+generate:
+	java -jar $(PLANTUML_JAR) $(FILE)
+
