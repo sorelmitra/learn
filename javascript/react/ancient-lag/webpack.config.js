4edd209c2ce38ead075fd838d17fc7b7a9009098
Sorel Mitra <sorelmitra@yahoo.com>
2025-07-04 12:15:31 +0300

    ancient-lag/add empty micro-frontend for single-spa

---
+++ javascript/react/ancient-lag/webpack.config.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "AcmeInc",
+    projectName: "AncientLag",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: false,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
