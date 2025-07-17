c24c8972a01b2491a95f6fc2cc704a0f07f678a5
Sorel Mitra <sorelmitra@yahoo.com>
2025-07-17 13:49:00 +0300

    single-spa/add start script and change layout

---
+++ javascript/single-spa/devsh-acme.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Starting a session
+# "devsh" stands for "Development Shell" -- a shell for running apps in development
+tmux new-session -d -s devsh-acme -n stuff
+
+# Starting a few services, each in its own window
+# Type <Prefix> : kill-session <Enter> in order to kill them all
+
+tmux new-window -n Acme-Root -c ~/1data/s/sorelmitra/learn/javascript/single-spa/acme-root
+tmux send-keys "yarn run start" Enter
+
+tmux new-window -n Ancient-Lag -c ~/1data/s/sorelmitra/learn/javascript/single-spa/ancient-lag
+tmux send-keys "yarn run start" Enter
+
+
+# Attach to the new session
+tmux attach
