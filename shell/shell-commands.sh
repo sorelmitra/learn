11dbe796562494d1f3a0c1190c64f11a01f34126
Sorel Mitra <sorelmitra@yahoo.com>
2025-08-01 17:44:12 +0300

    shell/fix command

--- shell/shell-commands.sh
+++ shell/shell-commands.sh
@@ -9,4 +9,4 @@
 # that change their value:
 # - PM_ID cycles through PMS based on the valu of "i"
 # - AMOUNT generates values like 13.05, where "5" is math computed from "i"
-COUNT=10 PMS=(pm_card_createDispute pm_card_createDisputeProductNotReceived pm_card_createDisputeInquiry); for (( i = 1; i <= $COUNT; i++ )); do; PM_ID=$PMS[$(( ( i - 1 ) % 3 + 1 ))] AMOUNT=`printf "%.2f\n" $(( ( 1300 + i ) / 100.0 ))` ENV=dev SCRIPT=~/1data/w/TQ/GL/gl-auto/pay/flows/transactions/flow_transaction_create_pay.py python $SCRIPT; echo "\n\n\n\n\n\n\n===\nDONE $ENV $PM_ID $AMOUNT $SCRIPT\n===\n\n\n\n"; done
+COUNT=10 PMS=(pm_card_createDispute pm_card_createDisputeProductNotReceived pm_card_createDisputeInquiry); for (( i = 1; i <= $COUNT; i++ )); do; pm_id=$PMS[$(( ( i - 1 ) % 3 + 1 ))] && amount=`printf "%.2f\n" $(( ( 1300 + i ) / 100.0 ))` && env=dev && script=~/1data/w/TQ/GL/gl-auto/pay/flows/transactions/flow_transaction_create_pay.py && ENV=$env PM_ID=$pm_id AMOUNT=$amount python $script; echo "\n\n\n\n\n\n\n===\nDONE $i / $COUNT -- $env $pm_id $amount $script\n===\n\n\n\nSleeping a bit ..."; sleep 10; done; echo "\n\n\n\nDONE $COUNT runs.\n"
